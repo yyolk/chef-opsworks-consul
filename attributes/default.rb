b1dfaaba990db1f0aab4293a9e03e6e05979477c
Adrien Brault <adrien.brault@gmail.com>
2014-11-20 19:30:45 +0100

    Fix attributes when cluster nodes already exist

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,13 +4,22 @@ default[:opsworks_consul][:layers][:bootstrap] = 'consul-bootstrap'
 set[:consul][:serve_ui] = true
 
 bootstrap_layer = default[:opsworks_consul][:layers][:bootstrap]
+cluster_layer = default[:opsworks_consul][:layers][:cluster]
 
 if node[:opsworks][:instance][:layers].include?(bootstrap_layer)
   set[:consul][:service_mode] = 'bootstrap'
 else
   set[:consul][:service_mode] = 'cluster'
   set[:consul][:bootstrap_expect] = 2 # make sure our cluster nodes are not started as bootstrap
-  set[:consul][:servers] = node[:opsworks][:layers][bootstrap_layer][:instances].map do |name, instance|
+
+  servers_layer = bootstrap_layer
+
+  # If there's already provisioned cluster nodes, use those instead of the bootstrap node
+  if node[:opsworks][:layers][cluster_layer][:instances].length > 0
+    servers_layer = cluster_layer
+  end
+
+  set[:consul][:servers] = node[:opsworks][:layers][servers_layer][:instances].map do |name, instance|
     instance[:private_ip]
   end
 end
